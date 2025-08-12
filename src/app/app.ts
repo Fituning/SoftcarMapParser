f48c7e7845c6ee72b7ed87a73f484dda5ea0a2f2
Sroÿce <63264339+Fituning@users.noreply.github.com>
2025-08-12 16:34:30 +0200

    remove home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
-import { Component, inject } from '@angular/core';
+import {Component, inject, NgZone, signal} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterOutlet, RouterLink, RouterLinkActive, Router} from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { FileStateService } from './shared/file-state.service';
 import { SidebarService } from './shared/sidebar.service';
 import { DeviceProfileService } from './shared/device-profile.service';
+import {MapEntry} from '../types/map-types';
+import {ParsedDataService} from './shared/parsed-data.service';
 
 @Component({
   selector: 'app-root',
@@ -18,6 +20,12 @@ export class App {
   sidebar   = inject(SidebarService);
   dev       = inject(DeviceProfileService);
   router    = inject(Router);
+  private fileState = inject(FileStateService);
+  private parsedStore = inject(ParsedDataService);
+  private zone = inject(NgZone);
+
+  parseMsg = signal<string>('');
+  parseOk  = signal<boolean | null>(null);
 
   saveCustom() {
     // On récupère les valeurs déjà mises dans le header
@@ -32,5 +40,40 @@ export class App {
         ram
       }
     });
+  }
+
+  async openAndParse() {
+    // 1) Choix du fichier (via Electron)
+    const filePath = await window.electronAPI.openMapDialog();
+    if (!filePath) {
+      this.parseMsg.set('Action annulée.');
+      this.parseOk.set(false);
+      return;
+    }
+
+    // 2) Parsing côté Node
+    const res = await window.electronAPI.parseMapFile(filePath);
+    if ((res as any)?.error) {
+      this.parsedStore.clear();
+      this.fileState.setFileName(null);
+      this.parseOk.set(false);
+      this.parseMsg.set(`❌ Erreur parsing: ${(res as any).error}`);
+      return;
+    }
+
+    // 3) Stockage + feedback
+    const entries = res as MapEntry[];
+    this.parsedStore.setEntries(entries);
+    const onlyName = filePath.split(/[\\/]/).pop() || filePath;
+    this.fileState.setFileName(onlyName);
+    this.parseOk.set(true);
+    this.parseMsg.set(`✅ Parsing OK — ${entries.length} entrées`);
+    this.zone.run(() => {
+      this.parsedStore.setEntries(entries);
+      const onlyName = filePath.split(/[\\/]/).pop() || filePath;
+      this.fileState.setFileName(onlyName);
+    });
+
+    // this.router.navigate(['/resume']); // todo fix the unshowed value at first loading
   }
 }
